cd3141743153f8d899d23cd1e3053f71e0b50e2a
Houssem Ben Mabrouk <houssem.benmabrouk.ext@orange.com>
2025-05-27 11:32:57 +0200

    feat: include severity+facility
    Signed-off-by: Houssem Ben Mabrouk <houssem.benmabrouk.ext@orange.com>

--- lib/fluent/plugin/formatter_syslog_rfc5424.rb
+++ lib/fluent/plugin/formatter_syslog_rfc5424.rb
@@ -1,4 +1,5 @@
 require 'rfc5424/formatter'
+require 'syslog'
 
 module Fluent
   module Plugin
@@ -12,6 +13,14 @@ module Fluent
       config_param :message_id_field, :string, default: "message_id"
       config_param :structured_data_field, :string, default: "structured_data"
       config_param :log_field, :string, default: "log"
+      config_param :facility_field, :string, default: "facility"
+      config_param :severity_field, :string, default: "severity"
+
+      DEFAULT_FACILITY = "user"
+      DEFAULT_SEVERITY = "info"
+
+      FACILITIES_MAP = {
+      }
 
       def configure(conf)
         super
@@ -21,13 +30,19 @@ module Fluent
         @message_id_field_array = @message_id_field.split(".")
         @structured_data_field_array = @structured_data_field.split(".")
         @log_field_array = @log_field.split(".")
+        @facility_field_array = @facility_field.split(".")
+        @severity_field_array = @severity_field.split(".")
       end
 
       def format(tag, time, record)
         log.debug("Record")
         log.debug(record.map { |k, v| "#{k}=#{v}" }.join('&'))
 
+        facility = record.dig(*@facility_field_array) || DEFAULT_FACILITY
+        severity = record.dig(*@severity_field_array) || DEFAULT_SEVERITY
+
         msg = RFC5424::Formatter.format(
+          prioiry: priority_from_facility_and_severity(facility, severity),
           log: record.dig(*@log_field_array) || "-",
           timestamp: time,
           hostname: record.dig(*@hostname_field_array) || "-",
@@ -43,6 +58,24 @@ module Fluent
         return msg + "\n" unless @rfc6587_message_size
 
         msg.length.to_s + ' ' + msg
+      end
+
+      def priority_from_facility_and_sieverity(facility=DEFAULT_FACILITY, severity=DEFAULT_SEVERITY)
+        begin
+          severity_int = Syslog.const_get("LOG_#{severity.upcase}")
+        rescue NameError
+          severity_int = Syslog.const_get("LOG_ALERT")
+        end
+        log.debug("Severity integer: #{severity_int}")
+
+        begin 
+          facility_int = Syslog.const_get("LOG_#{facility.upcase}")
+        rescue NameError
+          facility_int = Syslog.const_get("LOG_#{DEFAULT_FACILITY.upcase}")
+        end
+        log.debug("Facility integer: #{facility_int}")
+
+        return (facility_int * 8) + severity_int
       end
     end
   end
