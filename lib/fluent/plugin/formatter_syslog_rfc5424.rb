61acd89f6248e649a9828ec8851a0cd521cedb16
Houssem Ben Mabrouk <houssem.benmabrouk.ext@orange.com>
2026-06-17 15:48:32 +0200

    fix: encoding msg to utf-8
    Signed-off-by: Houssem Ben Mabrouk <houssem.benmabrouk.ext@orange.com>

--- lib/fluent/plugin/formatter_syslog_rfc5424.rb
+++ lib/fluent/plugin/formatter_syslog_rfc5424.rb
@@ -34,30 +34,57 @@ module Fluent
         @severity_field_array = @severity_field.split(".")
       end
 
+      def normalize_utf8(value)
+        str = value.to_s
+        return "-" if str.empty?
+
+        return str.encode('UTF-8') if str.encoding == Encoding::UTF_8 && str.valid_encoding?
+
+        utf8 = str.dup.force_encoding('UTF-8')
+        return utf8 if utf8.valid_encoding?
+
+        begin
+          str.encode('UTF-8', 'Windows-1252')
+        rescue
+          begin
+            str.encode('UTF-8', 'ISO-8859-1')
+          rescue
+            str.encode('UTF-8', invalid: :replace, undef: :replace, replace: '�')
+          end
+        end
+      end
+
       def format(tag, time, record)
-        log.debug("Record")
-        log.debug(record.map { |k, v| "#{k}=#{v}" }.join('&'))
+        log.debug("Record: #{record.map { |k, v| "#{k}=#{v}" }.join('&')}")
 
         facility = record.dig(*@facility_field_array) || DEFAULT_FACILITY
         severity = record.dig(*@severity_field_array) || DEFAULT_SEVERITY
 
+        log_value = normalize_utf8(record.dig(*@log_field_array) || "-")
+        hostname = normalize_utf8(record.dig(*@hostname_field_array) || "-")
+        app_name = normalize_utf8(record.dig(*@app_name_field_array) || "-")
+        proc_id = normalize_utf8(record.dig(*@proc_id_field_array) || "-")
+        msg_id = normalize_utf8(record.dig(*@message_id_field_array) || "-")
+        sd = normalize_utf8(record.dig(*@structured_data_field_array) || "-")
+
         msg = RFC5424::Formatter.format(
           priority: priority_from_facility_and_severity(facility, severity),
-          log: record.dig(*@log_field_array) || "-",
+          log: log_value,
           timestamp: time,
-          hostname: record.dig(*@hostname_field_array) || "-",
-          app_name: record.dig(*@app_name_field_array) || "-",
-          proc_id: record.dig(*@proc_id_field_array) || "-",
-          msg_id: record.dig(*@message_id_field_array) || "-",
-          sd: record.dig(*@structured_data_field_array) || "-"
+          hostname: hostname,
+          app_name: app_name,
+          proc_id: proc_id,
+          msg_id: msg_id,
+          sd: sd
         )
 
-        log.debug("RFC 5424 Message")
-        log.debug(msg)
+        msg = msg.to_s.encode("UTF-8", invalid: :replace, undef: :replace, replace: "�")
+
+        log.debug("RFC 5424 Message: #{msg}")  
 
         return msg + "\n" unless @rfc6587_message_size
 
-        msg.length.to_s + ' ' + msg
+        msg.bytesize.to_s + ' ' + msg
       end
 
       def priority_from_facility_and_severity(facility=DEFAULT_FACILITY, severity=DEFAULT_SEVERITY)
@@ -66,14 +93,14 @@ module Fluent
         rescue NameError
           severity_int = Syslog.const_get("LOG_ALERT")
         end
-        log.debug("Severity integer: #{severity_int}")
+        log.debug("Severity: #{severity_int}")
 
         begin
           facility_int = Syslog.const_get("LOG_#{facility.upcase}")
         rescue NameError
           facility_int = Syslog.const_get("LOG_#{DEFAULT_FACILITY.upcase}")
         end
-        log.debug("Facility integer: #{facility_int}")
+        log.debug("Facility: #{facility_int}")
 
         return (facility_int * 8) + severity_int
       end
